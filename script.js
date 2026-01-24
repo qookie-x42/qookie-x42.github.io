3807318605c81275e02e515ec5c3efba647bffdf
Qookie :) <davidalbu31@gmail.com>
2026-01-24 14:05:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const zoomImages = document.querySelectorAll('.zoomable');
-  const overlay = document.getElementById('overlay');
-  const overlayImg = document.getElementById('overlay-img');
+  const zoomImages = document.querySelectorAll(".zoomable");
+  const overlay = document.getElementById("overlay");
+  const overlayImg = document.getElementById("overlay-img");
 
   zoomImages.forEach(img => {
-    img.addEventListener('click', () => {
+    img.addEventListener("click", () => {
       overlayImg.src = img.src;
-      overlay.classList.add('active');
+      overlay.classList.add("active");
     });
   });
 
-  overlay.addEventListener('click', () => {
-    overlay.classList.remove('active');
+  overlay.addEventListener("click", () => {
+    overlay.classList.remove("active");
   });
 });
