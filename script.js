66258209ad90bda7c1d9663ffeb6e8e094d9fd83
Qookie :) <davidalbu31@gmail.com>
2026-01-29 13:25:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,101 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const overlay = document.querySelector("#overlay");
-  const overlayImg = document.querySelector("#overlay-img");
-  let key = "";
+	const overlay = document.querySelector("#overlay");
+	const overlayImg = document.querySelector("#overlay-img");
 
-  document.addEventListener("click", (e) => {
-    if (e.target.classList.contains("zoomable")) {
-      overlayImg.src = e.target.src;
-      if (overlayImg.src.includes("photo-rene-magritte.jpg")){
-        overlayImg.src = "walter-white.jpg";
-      }
-      else if (overlayImg.src.includes("magritte-image.webp")){
-        overlayImg.src = "jochen.jpg";
-      }
-      overlay.classList.add("active");
-    }
-    overlay.addEventListener("click", () => {
-      overlay.classList.remove("active");
-    });
-  });
+	document.addEventListener("click", zoomImage);
+	function zoomImage(e) {
+		if (e.target.classList.contains("zoomable")) {
+			overlayImg.src = e.target.src;
+			overlay.classList.add("active");
 
-  document.querySelectorAll(".heart").forEach(button => {
-    button.addEventListener("click", () => {
-      const art = button.closest(".p-art");
-      const id = self.crypto.randomUUID();
-      const img = art.querySelector("img").src;
-      const text = art.querySelector(".p-text");
-      const title = text.innerText;
-      const heart = button.querySelector("i");
-      let savedImg = JSON.parse(localStorage.getItem("likedPaintings")) || [];
-      const check = savedImg.find(item => item.id == id);
-      for(i=0;i<localStorage.length;i++){
-        key = savedImg[i].id;
-        if (key != check){
-          savedImg.push({ id, img, title });
-          localStorage.setItem("likedPaintings", JSON.stringify(savedImg));
-          heart.className = "fa fa-heart";
-          heart.style.color = "#f00";
-        }
-        else{
-          savedImg = savedImg.filter(item => item.id !== id);
-          localStorage.setItem("likedPaintings", JSON.stringify(savedImg));
-          heart.className = "fa fa-heart-o";
-          heart.style.color = "#000";
-        }
-      }
-    });
-  });
+			if (overlayImg.src.includes("photo-rene-magritte.jpg")) {
+				overlayImg.src = "walter-white.jpg";
+			}
+			else if (overlayImg.src.includes("magritte-image.webp")) {
+				overlayImg.src = "jochen.jpg";
+			}
+		}
+	}
 
-  if (window.location.pathname.includes("saved.html")) {
-    const page = document.querySelector("div#saved");
-    const loadImg = JSON.parse(localStorage.getItem("likedPaintings")) || [];
+	overlay.addEventListener("click", hideOverlay);
+	function hideOverlay() {
+		overlay.classList.remove("active");
+	}
 
-    if (loadImg.length == 0) {
-      const empty = document.createElement("p");
-      empty.className = "text";
-      page.appendChild(empty);
-      empty.innerText = " You haven't saved any images yet..."
-    }
+	document.querySelectorAll(".heart").forEach(button => {
+		button.addEventListener("click", () => savePainting(button));
+	});
+	/* document.querySelectorAll(".p-art").forEach(art => {
+		const img = art.querySelector("img").src;
+		const button = art.querySelector(".heart");
 
-    loadImg.forEach(item => {
-      const storedItem = document.createElement("div");
-      storedItem.className = "box p-art";
-      storedItem.style.width = "80%"
-      const storedImg = document.createElement("img");
-      storedImg.className = "p-art zoomable";
-      storedImg.src = item.img;
-      const storedText = document.createElement("div");
-      storedText.className = "text p-text";
-      storedText.innerText = item.title;
-      page.appendChild(storedItem);
-      storedItem.appendChild(storedImg);
-      storedItem.appendChild(storedText);
-    });
-  }
+		if (isPaintingSaved(img)) {
+			setHeartActive(button);
+		} 
+		else {
+			setHeartInactive(button);
+		}
+	}); */
+	function savePainting(button) {
+		const art = button.closest(".p-art");
+		const img = art.querySelector("img").src;
+		const title = art.querySelector(".p-text").innerText;
+		let savedImg = JSON.parse(localStorage.getItem("likedPaintings")) || [];
+		const exists = savedImg.find(item => item.img === img);
+
+		if (!exists) {
+			const id = self.crypto.randomUUID();
+			savedImg.push({ id, img, title });
+			setHeartActive(button)
+		}
+		else {
+			savedImg = savedImg.filter(item => item.img !== img);
+			setHeartInactive(button)
+		}
+		localStorage.setItem("likedPaintings", JSON.stringify(savedImg));
+	}
+
+	function setHeartActive(button) {
+		const heart = button.querySelector("i");
+		heart.className = "fa fa-heart";
+		heart.style.color = "#f00";
+	}
+
+	function setHeartInactive(button) {
+		const heart = button.querySelector("i");
+		heart.className = "fa fa-heart-o";
+		heart.style.color = "#000";
+	}
+
+	if (window.location.pathname.includes("saved.html")) {
+		loadPainting()
+	}
+	function loadPainting() {
+		const page = document.querySelector("div#saved");
+		const loadImg = JSON.parse(localStorage.getItem("likedPaintings")) || [];
+
+		if (loadImg.length == 0) {
+			const empty = document.createElement("p");
+			empty.className = "text";
+			page.appendChild(empty);
+			empty.innerText = "You haven't saved any images yet..."
+		}
+		loadImg.forEach(item => page.appendChild(createPainting(item)));
+	}
+	function createPainting() {
+		const storedItem = document.createElement("div");
+		storedItem.className = "box p-art";
+		storedItem.style.width = "80%"
+
+		const storedImg = document.createElement("img");
+		storedImg.className = "p-art zoomable";
+		storedImg.src = item.img;
+
+		const storedText = document.createElement("div");
+		storedText.className = "text p-text";
+		storedText.innerText = item.title;
+
+		storedItem.appendChild(storedImg, storedText);
+		return storedItem;
+	}
 });
